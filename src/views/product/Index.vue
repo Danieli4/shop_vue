827302c6131fffcc44d06ea91e5ac619c45c2820
danilgryaznev <daniel58912007@gmail.com>
2023-05-10 22:01:17 +0300

    Add display on group colors

--- src/views/product/Index.vue
+++ src/views/product/Index.vue
@@ -240,14 +240,14 @@
                                                                     class="flaticon-left-and-right-arrows"></i>
                                                                     <span>
                                                                             compare</span> </a> </li>
-                                                                <li><a href="#popup5" class="popup_link"> <i
+                                                                <li><a :href="`#popup${product.id}`" class="popup_link"> <i
                                                                     class="flaticon-visibility"></i>
                                                                     <span> quick view</span>
                                                                 </a> </li>
                                                             </ul>
                                                         </div>
                                                     </div>
-                                                    <div id="popup5" class="product-gird__quick-view-popup mfp-hide">
+                                                    <div :id="`popup${product.id}`" class="product-gird__quick-view-popup mfp-hide">
                                                         <div class="container">
                                                             <div class="row justify-content-between align-items-center">
                                                                 <div class="col-lg-6">
@@ -258,7 +258,7 @@
                                                                                     <li
                                                                                         class="tab-nav popup-product-thumb">
                                                                                         <a href="#tabb1">
-                                                                                            <img src="src/assets/images/shop/products-v6-img5.jpg"
+                                                                                            <img :src="product.image_url"
                                                                                                  alt="" /> </a> </li>
                                                                                     <li
                                                                                         class="tab-nav popup-product-thumb ">
@@ -277,7 +277,7 @@
                                                                                      class="tab-item popup-product-image">
                                                                                     <div
                                                                                         class="popup-product-single-image">
-                                                                                        <img src="src/assets/images/shop/products-v6-img5.jpg"
+                                                                                        <img :src="product.image_url"
                                                                                              alt="" /> </div>
                                                                                 </div>
                                                                                 <div id="tabb2"
@@ -304,7 +304,7 @@
                                                                 </div>
                                                                 <div class="col-lg-6">
                                                                     <div class="popup-right-content">
-                                                                        <h3>Brown Office Shoe</h3>
+                                                                        <h3>{{ product.title }}</h3>
                                                                         <div class="ratting"> <i
                                                                             class="flaticon-star"></i> <i
                                                                             class="flaticon-star"></i> <i
@@ -312,24 +312,19 @@
                                                                             <i class="flaticon-star"></i> <i
                                                                                 class="flaticon-star"></i>
                                                                             <span>(112)</span> </div>
-                                                                        <p class="text"> Hydrating Plumping Intense
-                                                                            Shine Lip Colour
+                                                                        <p class="text">{{ product.description }}
                                                                         </p>
                                                                         <div class="price">
-                                                                            <h2> $42 USD <del> $65 USD</del></h2>
+                                                                            <h2> ${{ product.price }} <del> $65 USD</del></h2>
                                                                             <h6> In stuck</h6>
                                                                         </div>
-                                                                        <div class="color-varient"> <a href="#0"
-                                                                                                       class="color-name pink">
-                                                                            <span>Pink</span> </a> <a href="#0"
-                                                                                                      class="color-name red">
-                                                                            <span>Red</span> </a>
-                                                                            <a href="#0"
-                                                                               class="color-name yellow"><span>Yellow</span>
-                                                                            </a> <a href="#0" class="color-name blue">
-                                                                                <span>Blue</span>
-                                                                            </a> <a href="#0" class="color-name black">
-                                                                                <span>Black</span> </a> </div>
+                                                                        <div class="color-varient">
+
+                                                                            <template v-for="groupProduct in product.group_products">
+
+                                                                            <a v-for="color in groupProduct.colors" href="#0" :style="`background:#${color.title};`" class="color-name pink"> <span>Pink</span> </a>
+                                                                            </template>
+                                                                        </div>
                                                                         <div class="add-product">
                                                                             <h6>Qty:</h6>
                                                                             <div class="button-group">
@@ -2140,6 +2135,10 @@ export default {
             this.axios.get('http://127.0.0.1/api/products')
                 .then(res=>{
                     this.products = res.data.data;
+                    console.log(res);
+                })
+                .finally(v=>{
+                    $(document).trigger('change')
                 })
         }
     }
